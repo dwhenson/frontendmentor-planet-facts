7d072d05769df9c1113daf6ce80de4634cc04e02
dwhenson <dave.henson@gmail.com>
2021-07-01 20:21:07 +0300

    ✅ ADD: Basic working JS functionality to hide/show text

--- notes.md
+++ notes.md
@@ -1,4 +1,4 @@
-# STEPS
+# TODO
 
 ## Partials
 
@@ -7,33 +7,49 @@
 
 -  Hamburger menu?
 
-<!-- 2. Pages -->
-<!-- - From front matter -->
-<!-- - Create planet collection -->
+2. Pages
+
+-  Pagination for pages using JSON file
+   <!-- - From front matter -->
+   <!-- - Create planet collection -->
 
 ## Styling and SCSS
 
 -  Work on main layout
-   -  Buttons at multiple viewports
-   -  TODO Add in numbered items
-   <!-- -  Work on details layout -->
+   <!-- -  Buttons at multiple viewports -->
+      <!-- -  Add in numbered items -->
+      <!-- -  Work on details layout -->
 -  Work on navigation layout
 -  Add in some transition smoothing
+-  Check where to add class in CSS for accents
 
 ## JS
 
 -  Burger menu
--  Add target selector and display for text (aria/tab order?)
+
+-  Buttons and content
+   UP-NEXT
+   Inits
+   -  Set all hidden text wtih JS - content and sources
+   -  Show first item in each array and add active class to button
+   -  Change link to a role of button?
+      App
+   -  Create updateContent function to hide/show as requried
+   -  Add comments to all sections
+   -  Refactor for common approach to locate target item in array
 
 # FIXME
 
--  Text wraping on bottom flex
--  Seperator is on top of underline in mobile view
-<!-- -  Border causing jank on mobile links, and impacted by border radius -->
+-  Text wrapping on bottom flex
+-  Different text on buttons/viewport
+   <!-- -  Separator is on top of underline in mobile view -->
+   <!-- -  Border causing jank on mobile links, and impacted by border radius -->
 
 # ReadMe
 
-1. Many pages so some form of templating needed - SSG 11th and nunjucks. AB 11ty course with some other additions
-2. Data in json. I split into seperate md files rather than iterate over object and render - best? Works well with 11th - easy to add new planets!
-3. Center section layout was a challenge, wanted to keep source and tab order logical. Concluded a tabbed interface? Heydon Pickering resource.
+1. Many pages so some form of templating needed - SSG 11ty and nunjucks. AB 11ty course with some other additions
+   -  Nav menu with JS helper
+   -  Data for pages with pagination
+2. Data in json. I split into separate md files rather than iterate over object and render - best? Works well with 11th - easy to add new planets!
+3. Center section layout was a challenge, wanted to keep source and tab order logical. Concluded a tabbed interface not any more? Tried/Failed with CSS
 4. Setting a custom property in the document head using nunjunks - is that a good idea? What about CP scope?
--- src/js/inits.js
+++ src/js/inits.js
@@ -2,53 +2,81 @@ const contents = [...document.querySelectorAll("[data-content]")];
 const buttons = [...document.querySelectorAll("[data-button]")];
 const sources = [...document.querySelectorAll("[data-source]")];
 
+/**
+ * Updates the page content based on the button clicked
+ * @param      {event}  event   The event
+ * @param      {array}  array   The array of content items
+ */
 function updateContent(event, array) {
+	// Hide all the items in the array
 	array.forEach(function (item) {
 		if (item.style.visibility === "visible") {
 			item.style.visibility = "hidden";
 		}
 	});
-}
 
-function updateButtons(event, array) {
-	array.forEach(function (item) {
-		if (item.classList.contains("active")) {
-			item.classList.remove("active");
-		}
+	// Return and show the relevant link
+	const contentToShow = array.find((item) => {
+		return item.dataset.content === event.target.closest("[data-button]").dataset.button;
 	});
-	event.target.closest("[data-button]").classList.add("active");
+	contentToShow.style.visibility = "visible";
 }
 
+/**
+ * Updates the link to the Wikipedia link based on the button clicked
+ * @param      {event}  event   The event
+ * @param      {array}  array   The array of links
+ */
 function updateSources(event, array) {
+	// Hide all items in the array
 	array.forEach(function (item) {
 		if (item.style.display === "inline") {
 			item.style.display = "none";
 		}
 	});
-	console.log(event.target.closest("[data-button]").dataset.button);
-
+	// Return and show the relevant link
 	const sourceToShow = array.find((item) => {
 		return item.dataset.source === event.target.closest("[data-button]").dataset.button;
 	});
 	sourceToShow.style.display = "inline";
 }
 
+/**
+ * Finds the clicked button and highlight in the UI
+ * @param      {event}  event   The event
+ * @param      {array}  array   The array of buttons
+ */
+function updateButtons(event, array) {
+	array.forEach(function (item) {
+		if (item.classList.contains("js-active")) {
+			item.classList.remove("js-active");
+		}
+	});
+	event.target.closest("[data-button]").classList.add("js-active");
+}
+
+/* Handlers
+/* ==================================================== */
+/**
+ * Call functions to render the UI updates
+ * @param      {<type>}  event   The event
+ */
 function handleClicks(event) {
 	if (!event.target.closest("[data-button]")) return;
 	updateButtons(event, buttons);
-	// updateContent(event, contents);
+	updateContent(event, contents);
 	updateSources(event, sources);
 }
 
+/* ====================================================
+   Inits and Event Listeners
+   ==================================================== */
+
 function showInitialContent() {
-	// contents[0].style.visibility = "visible";
-	document.location.hash = "#overview";
-	buttons[0].classList.add("active");
+	contents[0].style.visibility = "visible";
+	buttons[0].classList.add("js-active");
 	sources[0].style.display = "inline";
 }
 
 document.addEventListener("click", handleClicks);
 showInitialContent();
-
-// window.onload = function () {
-// };
