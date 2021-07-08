9377f8da6a7fa2b6eefc0c78b3642916c9b43130
dwhenson <dave.henson@gmail.com>
2021-07-08 08:51:33 +0300

    ✅ ADD: Production files

---
+++ dist/.eleventy.js
@@ -0,0 +1,24 @@
+module.exports = (config) => {
+  // config.addPassthroughCopy("./src/images/");
+
+  // Disable browsersync
+  config.setBrowserSyncConfig({
+    snippetOptions: {
+      // Regex that will never match, so snippet never added.
+      rule: {
+        match: /no-such-element/,
+      },
+    },
+  });
+
+  return {
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+  };
+};
--- dist/_data/helpers.js
+++
@@ -1,2 +0,0 @@
-var helpers=function(){"use strict";return{getLinkActiveState(e,t){let r="";return e===t&&(r=' aria-current="page"'),r}}}();
-//# sourceMappingURL=helpers.js.map
