d36ea8a6dc46912e831b1d0dfe792e1c222b1e14
dwhenson <dave.henson@gmail.com>
2021-07-09 16:43:23 +0300

    🐛 FIX: Update outline styles and numbering

--- dist/js/inits.js
+++ dist/js/inits.js
@@ -1,2 +1,284 @@
-!function(){"use strict";const t=[...document.querySelectorAll("[data-content]")],e=[...document.querySelectorAll("[data-button]")],s=[...document.querySelectorAll("[data-source]")],i=[...document.querySelectorAll("[data-image]")],a=document.querySelector(".sources");class n extends HTMLElement{constructor(){super();const t=this;this.state=new Proxy({status:"open",enabled:!1},{set(e,s,i){const a=e[s];return e[s]=i,a!==i&&t.processStateChange(),e}})}get maxWidth(){return parseInt(this.getAttribute("max-width")||9999,10)}connectedCallback(){this.initialMarkup=this.innerHTML,this.render();new ResizeObserver((t=>{const{contentRect:e}=t[0];this.state.enabled=e.width<=this.maxWidth})).observe(this.parentNode)}render(){this.innerHTML=`\n\t\t\t<div class="burger-menu" data-element="burger-root">\n\t\t\t\t<button class="burger-menu__trigger" data-element="burger-menu-trigger" type="button" aria-label="Open menu">\n\t\t\t\t\t<span class="burger-menu__bar" aria-hidden="true"></span>\n\t\t\t\t</button>\n\t\t\t\t<div class="burger-menu__panel" data-element="burger-menu-panel">\n\t\t\t\t\t${this.initialMarkup}\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t`,this.postRender()}postRender(){var t;if(this.trigger=this.querySelector('[data-element="burger-menu-trigger"]'),this.panel=this.querySelector('[data-element="burger-menu-panel"]'),this.root=this.querySelector('[data-element="burger-root"]'),this.focusableElements=(t=this)?t.querySelectorAll('button:not([disabled]), [href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), [tabindex]:not([tabindex="-1"]):not([disabled]), details:not([disabled]), summary:not(:disabled)'):(console.warn("You need to pass a parent HTMLElement"),[]),this.trigger&&this.panel)return this.toggle(),this.trigger.addEventListener("click",(t=>{t.preventDefault(),this.toggle()})),void document.addEventListener("focusin",(()=>{this.contains(document.activeElement)||this.toggle("closed")}));this.innerHTML=this.initialMarkup}toggle(t){if(t){if(this.state.status===t)return;this.state.status=t}else this.state.status="closed"===this.state.status?"open":"closed"}processStateChange(){switch(this.root.setAttribute("status",this.state.status),this.root.setAttribute("enabled",this.state.enabled?"true":"false"),this.manageFocus(),this.state.status){case"closed":this.trigger.setAttribute("aria-expanded","false"),this.trigger.setAttribute("aria-label","Open menu");break;case"open":case"initial":this.trigger.setAttribute("aria-expanded","true"),this.trigger.setAttribute("aria-label","Close menu");break;default:console.log("Error Process State Change")}}manageFocus(){if(this.state.enabled)switch(this.state.status){case"open":this.focusableElements.forEach((t=>t.removeAttribute("tabindex")));break;case"closed":[...this.focusableElements].filter((t=>"burger-menu-trigger"!==t.getAttribute("data-element"))).forEach((t=>t.setAttribute("tabindex","-1")));break;default:console.log("Error Manage Focus")}else this.focusableElements.forEach((t=>t.removeAttribute("tabindex")))}}"customElements"in window&&customElements.define("burger-menu",n),a.style.display="block",t[0].style.visibility="visible",e[0].classList.add("js-active"),s[0].style.display="inline",i[0].style.visibility="visible",document.addEventListener("click",(function(a){a.target.closest("[data-button]")&&(a.preventDefault(),function(t,e){e.forEach((function(t){t.classList.contains("js-active")&&t.classList.remove("js-active")})),t.target.closest("[data-button]").classList.add("js-active")}(a,e),function(t,e){e.forEach((function(t){"visible"===t.style.visibility&&(t.style.visibility="hidden")})),e.find((e=>e.dataset.content===t.target.closest("[data-button]").dataset.button)).style.visibility="visible"}(a,t),function(t,e){e.forEach((function(t){"inline"===t.style.display&&(t.style.display="none")})),e.find((e=>e.dataset.source===t.target.closest("[data-button]").dataset.button)).style.display="inline"}(a,s),function(t,e){if(t.target.closest("[data-button='geology']")){const t=e.find((t=>"geology"===t.dataset.image));return"visible"===t.style.visibility&&(t.style.visibility="hidden"),void(t.style.visibility="visible")}e.forEach((function(t){"visible"===t.style.visibility&&(t.style.visibility="hidden")})),e.find((e=>e.dataset.image===t.target.closest("[data-button]").dataset.button)).style.visibility="visible"}(a,i))}))}();
-//# sourceMappingURL=inits.js.map
+(function () {
+  'use strict';
+
+  const contents = [...document.querySelectorAll("[data-content]")];
+  const buttons = [...document.querySelectorAll("[data-button]")];
+  const sources = [...document.querySelectorAll("[data-source]")];
+  const images = [...document.querySelectorAll("[data-image]")];
+  const sourcesParent = document.querySelector(".sources");
+
+  /**
+   * Finds the clicked button and highlight in the UI
+   * @param      {event}  event   The event
+   * @param      {array}  array   The array of buttons
+   */
+  function updateButtons(event, array) {
+    array.forEach(function (item) {
+      if (item.classList.contains("js-active")) {
+        item.classList.remove("js-active");
+      }
+    });
+    event.target.closest("[data-button]").classList.add("js-active");
+  }
+
+  /**
+   * Updates the page content based on the button clicked
+   * @param      {event}  event   The event
+   * @param      {array}  array   The array of content items
+   */
+  function updateContent(event, array) {
+    // Hide all the items in the array
+    array.forEach(function (item) {
+      if (item.style.visibility === "visible") {
+        item.style.visibility = "hidden";
+      }
+    });
+
+    // Return and show the relevant link
+    const contentToShow = array.find((item) => {
+      return item.dataset.content === event.target.closest("[data-button]").dataset.button;
+    });
+    contentToShow.style.visibility = "visible";
+  }
+
+  /**
+   * Updates the link to the Wikipedia link based on the button clicked
+   * @param      {event}  event   The event
+   * @param      {array}  array   The array of links
+   */
+  function updateSources(event, array) {
+    // Hide all items in the array
+    array.forEach(function (item) {
+      if (item.style.display === "inline") {
+        item.style.display = "none";
+      }
+    });
+    // Return and show the relevant link
+    const sourceToShow = array.find((item) => {
+      return item.dataset.source === event.target.closest("[data-button]").dataset.button;
+    });
+    sourceToShow.style.display = "inline";
+  }
+
+  /**
+   * Updates the image based on the button clicked
+   * @param      {event}  event   The event
+   * @param      {array}  array   The array of images
+   */
+  function updateImage(event, array) {
+    // If the geology button is clicked
+    if (event.target.closest("[data-button='geology']")) {
+      // Get the image, hide/show and return
+      const showImage = array.find((item) => {
+        return item.dataset.image === "geology";
+      });
+      if (showImage.style.visibility === "visible") {
+        showImage.style.visibility = "hidden";
+      }
+      showImage.style.visibility = "visible";
+      return;
+    }
+    // Otherwise, hide all the items in the array
+    array.forEach(function (item) {
+      if (item.style.visibility === "visible") {
+        item.style.visibility = "hidden";
+      }
+    });
+    // Return and show the relevant image
+    const imageToShow = array.find((item) => {
+      return item.dataset.image === event.target.closest("[data-button]").dataset.button;
+    });
+    imageToShow.style.visibility = "visible";
+  }
+
+  /**
+   * Call functions to render the UI updates
+   * @param      {<type>}  event   The event
+   */
+  function handleClicks(event) {
+     if (!event.target.closest("[data-button]")) return;
+     event.preventDefault();
+     updateButtons(event, buttons);
+     updateContent(event, contents);
+     updateSources(event, sources);
+     updateImage(event, images);
+  }
+
+  /**
+   * Returns back a NodeList of focusable elements
+   * that exist within the passed parent HTMLElement
+   *
+   * @param {HTMLElement} parent HTML element
+   * @returns {NodeList} The focusable elements that we can find
+   */
+  var getFocusableElements = (parent) => {
+  	if (!parent) {
+  		console.warn("You need to pass a parent HTMLElement");
+  		return [];
+  	}
+
+  	return parent.querySelectorAll(
+  		'button:not([disabled]), [href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), [tabindex]:not([tabindex="-1"]):not([disabled]), details:not([disabled]), summary:not(:disabled)'
+  	);
+  };
+
+  class BurgerMenu extends HTMLElement {
+  	constructor() {
+  		super();
+
+  		const self = this;
+
+  		this.state = new Proxy(
+  			{
+  				status: "open",
+  				enabled: false,
+  			},
+  			{
+  				set(state, key, value) {
+  					const oldValue = state[key];
+
+  					state[key] = value;
+  					if (oldValue !== value) {
+  						self.processStateChange();
+  					}
+  					return state;
+  				},
+  			}
+  		);
+  	}
+
+  	get maxWidth() {
+  		return parseInt(this.getAttribute("max-width") || 9999, 10);
+  	}
+
+  	connectedCallback() {
+  		this.initialMarkup = this.innerHTML;
+  		this.render();
+
+  		const observer = new ResizeObserver((observedItems) => {
+  			const { contentRect } = observedItems[0];
+  			this.state.enabled = contentRect.width <= this.maxWidth;
+  		});
+
+  		// We want to watch the parent like a hawk
+  		observer.observe(this.parentNode);
+  	}
+
+  	render() {
+  		this.innerHTML = `
+			<div class="burger-menu" data-element="burger-root">
+				<button class="burger-menu__trigger" data-element="burger-menu-trigger" type="button" aria-label="Open menu">
+					<span class="burger-menu__bar" aria-hidden="true"></span>
+				</button>
+				<div class="burger-menu__panel" data-element="burger-menu-panel">
+					${this.initialMarkup}
+				</div>
+			</div>
+		`;
+
+  		this.postRender();
+  	}
+
+  	postRender() {
+  		this.trigger = this.querySelector('[data-element="burger-menu-trigger"]');
+  		this.panel = this.querySelector('[data-element="burger-menu-panel"]');
+  		this.root = this.querySelector('[data-element="burger-root"]');
+  		this.focusableElements = getFocusableElements(this);
+
+  		if (this.trigger && this.panel) {
+  			this.toggle();
+
+  			this.trigger.addEventListener("click", (evt) => {
+  				evt.preventDefault();
+
+  				this.toggle();
+  			});
+
+  			document.addEventListener("focusin", () => {
+  				if (!this.contains(document.activeElement)) {
+  					this.toggle("closed");
+  				}
+  			});
+
+  			return;
+  		}
+
+  		this.innerHTML = this.initialMarkup;
+  	}
+
+  	toggle(forcedStatus) {
+  		if (forcedStatus) {
+  			if (this.state.status === forcedStatus) {
+  				return;
+  			}
+
+  			this.state.status = forcedStatus;
+  		} else {
+  			this.state.status = this.state.status === "closed" ? "open" : "closed";
+  		}
+  	}
+
+  	processStateChange() {
+  		this.root.setAttribute("status", this.state.status);
+  		this.root.setAttribute("enabled", this.state.enabled ? "true" : "false");
+
+  		this.manageFocus();
+
+  		switch (this.state.status) {
+  			case "closed":
+  				this.trigger.setAttribute("aria-expanded", "false");
+  				this.trigger.setAttribute("aria-label", "Open menu");
+  				break;
+  			case "open":
+  			case "initial":
+  				this.trigger.setAttribute("aria-expanded", "true");
+  				this.trigger.setAttribute("aria-label", "Close menu");
+  				break;
+  			default:
+  				console.log("Error Process State Change");
+  		}
+  	}
+
+  	manageFocus() {
+  		if (!this.state.enabled) {
+  			this.focusableElements.forEach((element) => element.removeAttribute("tabindex"));
+  			return;
+  		}
+
+  		switch (this.state.status) {
+  			case "open":
+  				this.focusableElements.forEach((element) => element.removeAttribute("tabindex"));
+  				break;
+  			case "closed":
+  				[...this.focusableElements]
+  					.filter(
+  						(element) => element.getAttribute("data-element") !== "burger-menu-trigger"
+  					)
+  					.forEach((element) => element.setAttribute("tabindex", "-1"));
+  				break;
+  			default:
+  				console.log("Error Manage Focus");
+  		}
+  	}
+  }
+
+  if ("customElements" in window) {
+  	customElements.define("burger-menu", BurgerMenu);
+  }
+
+  /**
+   * Shows the initial content for the main section
+   */
+  function showInitialContent() {
+     sourcesParent.style.display = "block";
+     contents[0].style.visibility = "visible";
+     buttons[0].classList.add("js-active");
+     sources[0].style.display = "inline";
+     images[0].style.visibility = "visible";
+  }
+
+  // Inits
+  showInitialContent();
+  document.addEventListener("click", handleClicks);
+
+}());
--- notes.md
+++ notes.md
@@ -3,33 +3,11 @@
 ## Structure and 11ty
 
 -  Pagination for pages using JSON file
+-  Render all images either through markdown or 11ty and pagination
+-  Update font loading strategy
 
 ## FIXME
 
--  Render all images either through markdown or 11ty and pagination
-   <!-- -  Put surface image back in flow to prevent overflow -->
-   <!-- -  Work on nav panel when open - 100vh? And no scroll? -->
-
-# README
-
-## Key points
-
-1. Many pages so some form of templating needed - SSG 11ty and nunjucks. AB 11ty course with some other additions
-   -  Nav menu with JS helper
-   -  Data for pages with pagination
-2. Data in json. I split into separate md files rather than iterate over object and render - best? Works well with 11th - easy to add new planets!
-3. Center section layout was a challenge, wanted to keep source and tab order logical. Concluded a tabbed interface not any more? Tried/Failed with CSS
-4. Setting a custom property in the document head using nunjunks - is that a good idea? Use inline styling to use the cascade to apply different accents underneath to each `li`
-
-## A11y
-
--  Source code order and tab order - a bit contradictory - but the details in a list inside the `aside` mainly as it has a built in landmark and seemed more appropriate than `footer`
--  Would have liked to have the links in the buttons fill content so outline styles better
-   -  Possible solution with JS, but if JS fails have links that are not keyboard focusable, have to set attributes in JS on init - seemed like a downward spiral...
-
-## Progressive Enhancement
-
--  No CSS - not ideal as headers and content are separate but works, just about.
--  No JS
-   -  Burger menu
-   -  Showing content - would have liked to render some initial text cannot make it work no matter what combination of selectors I tried!
+-  Outline styles for buttons - remove list styles, hard code and set a to block
+-  Add some spacing to the aside buttom
+-  Prevent scrolling on body when burger menu is open?
